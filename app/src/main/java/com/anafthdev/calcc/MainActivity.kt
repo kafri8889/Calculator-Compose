8b5307d08fb4c08fb81c276274aa59635123a9b2
kafri8889 <kafri8889@gmail.com>
2022-05-28 18:01:57 +0700

    refactor: add surface to main screen

--- app/src/main/java/com/anafthdev/calcc/MainActivity.kt
+++ app/src/main/java/com/anafthdev/calcc/MainActivity.kt
@@ -3,13 +3,8 @@ package com.anafthdev.calcc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import com.anafthdev.calcc.ui.main.MainScreen
 import com.anafthdev.calcc.ui.theme.CalcCTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,7 +15,11 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			CalcCTheme {
-				MainScreen()
+				Surface(
+					color = MaterialTheme.colorScheme.background
+				) {
+					MainScreen()
+				}
 			}
 		}
 	}
