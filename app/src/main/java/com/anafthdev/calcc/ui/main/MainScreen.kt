6c3afcffbb4f73921ec4e59543a5809c9a234296
kafri8889 <kafri8889@gmail.com>
2022-05-28 18:05:27 +0700

    refactor: UI update
    add interaction source to CalcC button box, add horizontal scroll to expression text, change system bar color, ect

--- app/src/main/java/com/anafthdev/calcc/ui/main/MainScreen.kt
+++ app/src/main/java/com/anafthdev/calcc/ui/main/MainScreen.kt
@@ -1,18 +1,22 @@
 package com.anafthdev.calcc.ui.main
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.anafthdev.calcc.R
 import com.anafthdev.calcc.data.Calc
@@ -20,10 +24,13 @@ import com.anafthdev.calcc.data.CalcCAction
 import com.anafthdev.calcc.data.CalcCButtons
 import com.anafthdev.calcc.foundation.extension.split
 import com.anafthdev.calcc.ui.theme.fully_rounded
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+	
+	val backgroundColorScheme = MaterialTheme.colorScheme.background
 	
 	val config = LocalConfiguration.current
 	
@@ -32,6 +39,19 @@ fun MainScreen() {
 	val state by mainViewModel.state.collectAsState()
 	
 	val expression = state.expression
+	
+	val systemUiController = rememberSystemUiController()
+	val expressionTextScrollState = rememberScrollState()
+	
+	LaunchedEffect(expression) {
+		expressionTextScrollState.animateScrollTo(0)
+	}
+	
+	SideEffect {
+		systemUiController.setSystemBarsColor(
+			color = backgroundColorScheme
+		)
+	}
 	
 	Column(
 		modifier = Modifier
@@ -47,8 +67,69 @@ fun MainScreen() {
 						.dp
 				)
 		) {
-			Text(
-				text = expression
+			Box(
+				modifier = Modifier
+					.fillMaxHeight()
+					.width(
+						config.screenWidthDp
+							.div(9)
+							.dp
+					)
+					.background(
+						brush = Brush.horizontalGradient(
+							listOf(
+								Color.Transparent,
+								backgroundColorScheme
+							)
+						)
+					)
+					.zIndex(2f)
+					.align(Alignment.CenterEnd)
+			)
+			
+			Column(
+				horizontalAlignment = Alignment.End,
+				modifier = Modifier
+			) {
+				Text(
+					text = expression,
+					style = MaterialTheme.typography.headlineLarge,
+					modifier = Modifier
+						.horizontalScroll(
+							state = expressionTextScrollState,
+							reverseScrolling = true
+						)
+						.padding(
+							horizontal = 24.dp
+						)
+				)
+				
+				Text(
+					text = expression,
+					style = MaterialTheme.typography.headlineSmall.copy(
+						color = MaterialTheme.typography.headlineSmall.color.copy(alpha = 0.6f)
+					)
+				)
+			}
+			
+			Box(
+				modifier = Modifier
+					.fillMaxHeight()
+					.width(
+						config.screenWidthDp
+							.div(9)
+							.dp
+					)
+					.background(
+						brush = Brush.horizontalGradient(
+							listOf(
+								backgroundColorScheme,
+								Color.Transparent
+							)
+						)
+					)
+					.zIndex(2f)
+					.align(Alignment.CenterStart)
 			)
 		}
 		
@@ -69,6 +150,16 @@ fun MainScreen() {
 				val r3 = list[2]!!
 				val r4 = list[3]!!
 				
+				val r1InteractionSource = remember { MutableInteractionSource() }
+				val r2InteractionSource = remember { MutableInteractionSource() }
+				val r3InteractionSource = remember { MutableInteractionSource() }
+				val r4InteractionSource = remember { MutableInteractionSource() }
+				
+				val updateExpressionR1 = { updateExpression(mainViewModel, expression, r1) }
+				val updateExpressionR2 = { updateExpression(mainViewModel, expression, r2) }
+				val updateExpressionR3 = { updateExpression(mainViewModel, expression, r3) }
+				val updateExpressionR4 = { updateExpression(mainViewModel, expression, r4) }
+				
 				Row(
 					verticalAlignment = Alignment.CenterVertically,
 					horizontalArrangement = Arrangement.Center,
@@ -79,21 +170,22 @@ fun MainScreen() {
 						contentAlignment = Alignment.Center,
 						modifier = Modifier
 							.weight(1f, fill = true)
+							.clickable(
+								enabled = true,
+								indication = null,
+								interactionSource = r1InteractionSource,
+								onClick = updateExpressionR1
+							)
 					) {
 						Card(
 							shape = fully_rounded,
+							interactionSource = r1InteractionSource,
 							colors = CardDefaults.cardColors(
 								containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
 									enabled = true
 								).value else Color.Transparent
 							),
-							onClick = {
-								mainViewModel.dispatch(
-									MainAction.UpdateExpression(
-										mainViewModel.getExpression(expression, r1)
-									)
-								)
-							},
+							onClick = updateExpressionR1,
 							modifier = Modifier
 								.padding(8.dp)
 								.size(56.dp)
@@ -116,22 +208,23 @@ fun MainScreen() {
 					Box(
 						contentAlignment = Alignment.Center,
 						modifier = Modifier
-							.weight(1f, fill = true),
+							.weight(1f, fill = true)
+							.clickable(
+								enabled = true,
+								indication = null,
+								interactionSource = r2InteractionSource,
+								onClick = updateExpressionR2
+							)
 					) {
 						Card(
 							shape = fully_rounded,
+							interactionSource = r2InteractionSource,
 							colors = CardDefaults.cardColors(
 								containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
 									enabled = true
 								).value else Color.Transparent
 							),
-							onClick = {
-								mainViewModel.dispatch(
-									MainAction.UpdateExpression(
-										mainViewModel.getExpression(expression, r2)
-									)
-								)
-							},
+							onClick = updateExpressionR2,
 							modifier = Modifier
 								.padding(8.dp)
 								.size(56.dp)
@@ -154,22 +247,23 @@ fun MainScreen() {
 					Box(
 						contentAlignment = Alignment.Center,
 						modifier = Modifier
-							.weight(1f, fill = true),
+							.weight(1f, fill = true)
+							.clickable(
+								enabled = true,
+								indication = null,
+								interactionSource = r3InteractionSource,
+								onClick = updateExpressionR3
+							)
 					) {
 						Card(
 							shape = fully_rounded,
+							interactionSource = r3InteractionSource,
 							colors = CardDefaults.cardColors(
 								containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
 									enabled = true
 								).value else Color.Transparent
 							),
-							onClick = {
-								mainViewModel.dispatch(
-									MainAction.UpdateExpression(
-										mainViewModel.getExpression(expression, r3)
-									)
-								)
-							},
+							onClick = updateExpressionR3,
 							modifier = Modifier
 								.padding(8.dp)
 								.size(56.dp)
@@ -199,18 +293,19 @@ fun MainScreen() {
 					Box(
 						contentAlignment = Alignment.Center,
 						modifier = Modifier
-							.weight(1f, fill = true),
+							.weight(1f, fill = true)
+							.clickable(
+								enabled = true,
+								indication = null,
+								interactionSource = r4InteractionSource,
+								onClick = updateExpressionR4
+							)
 					) {
 						Card(
 							shape = fully_rounded,
+							interactionSource = r4InteractionSource,
 							colors = CardDefaults.cardColors(),
-							onClick = {
-								mainViewModel.dispatch(
-									MainAction.UpdateExpression(
-										mainViewModel.getExpression(expression, r4)
-									)
-								)
-							},
+							onClick = updateExpressionR4,
 							modifier = Modifier
 								.padding(8.dp)
 								.size(56.dp)
@@ -239,4 +334,16 @@ fun MainScreen() {
 			)
 		}
 	}
+}
+
+private fun updateExpression(
+	mainViewModel: MainViewModel,
+	exp: String,
+	calc: Calc
+) {
+	mainViewModel.dispatch(
+		MainAction.UpdateExpression(
+			mainViewModel.getExpression(exp, calc)
+		)
+	)
 }
