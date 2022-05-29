5fbe31717827f3d7b051b265e7099c8deccf425e
kafri8889 <kafri8889@gmail.com>
2022-05-29 16:35:03 +0700

    refactor: UI update

--- app/src/main/java/com/anafthdev/calcc/ui/main/MainScreen.kt
+++ app/src/main/java/com/anafthdev/calcc/ui/main/MainScreen.kt
@@ -1,16 +1,23 @@
 package com.anafthdev.calcc.ui.main
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -47,6 +54,8 @@ fun MainScreen() {
 	
 	val systemUiController = rememberSystemUiController()
 	val expressionTextScrollState = rememberScrollState()
+	
+	var isAdvancedButtonExpanded by remember { mutableStateOf(false) }
 	
 	LaunchedEffect(expression) {
 		expressionTextScrollState.animateScrollTo(0)
@@ -141,166 +150,120 @@ fun MainScreen() {
 			)
 		}
 		
-		Column(
+		Spacer(
 			modifier = Modifier
 				.weight(1f)
+		)
+		
+		Column(
+			modifier = Modifier
 				.background(MaterialTheme.colorScheme.tertiaryContainer.copy(alpha = 0.18f))
 		) {
-			Box(
+			IconButton(
+				onClick = {
+					isAdvancedButtonExpanded = !isAdvancedButtonExpanded
+				},
 				modifier = Modifier
-					.padding(8.dp)
-					.size(
-						width = 32.dp,
-						height = 4.dp
+					.padding(4.dp)
+					.rotate(
+						if (isAdvancedButtonExpanded) 180f else 360f
 					)
-					.clip(fully_rounded)
-					.background(Color.Gray)
 					.align(Alignment.CenterHorizontally)
-			)
+			) {
+				Icon(
+					imageVector = Icons.Rounded.ArrowDropDown,
+					contentDescription = null
+				)
+			}
+			
+			AnimatedVisibility(
+				visible = isAdvancedButtonExpanded,
+				enter = expandVertically(),
+				exit = shrinkVertically()
+			) {
+				Column(
+					modifier = Modifier
+						.fillMaxWidth()
+						.height(96.dp)
+						.background(Color.Black)
+				) {
+				
+				}
+			}
 			
 			Spacer(
 				modifier = Modifier
-					.weight(1f)
+					.padding(4.dp)
 			)
 			
-			CalcCButtons.values.split(4).second.forEachIndexed { i, list ->
-				
-				val r1 = list[0]!!
-				val r2 = list[1]!!
-				val r3 = list[2]!!
-				val r4 = list[3]!!
-				
-				val r1InteractionSource = remember { MutableInteractionSource() }
-				val r2InteractionSource = remember { MutableInteractionSource() }
-				val r3InteractionSource = remember { MutableInteractionSource() }
-				val r4InteractionSource = remember { MutableInteractionSource() }
-				
-				val updateExpressionR1 = { updateExpression(mainViewModel, expression, r1) }
-				val updateExpressionR2 = { updateExpression(mainViewModel, expression, r2) }
-				val updateExpressionR3 = { updateExpression(mainViewModel, expression, r3) }
-				val updateExpressionR4 = { updateExpression(mainViewModel, expression, r4) }
-				
-				Row(
-					verticalAlignment = Alignment.CenterVertically,
-					horizontalArrangement = Arrangement.Center,
-					modifier = Modifier
-						.fillMaxWidth()
-				) {
-					Box(
-						contentAlignment = Alignment.Center,
+			Column(
+				modifier = Modifier.fillMaxWidth()
+			) {
+				CalcCButtons.values.split(4).second.forEachIndexed { i, list ->
+					
+					val r1 = list[0]!!
+					val r2 = list[1]!!
+					val r3 = list[2]!!
+					val r4 = list[3]!!
+					
+					val r1InteractionSource = remember { MutableInteractionSource() }
+					val r2InteractionSource = remember { MutableInteractionSource() }
+					val r3InteractionSource = remember { MutableInteractionSource() }
+					val r4InteractionSource = remember { MutableInteractionSource() }
+					
+					val updateExpressionR1 = { updateExpression(mainViewModel, expression, r1) }
+					val updateExpressionR2 = { updateExpression(mainViewModel, expression, r2) }
+					val updateExpressionR3 = { updateExpression(mainViewModel, expression, r3) }
+					val updateExpressionR4 = { updateExpression(mainViewModel, expression, r4) }
+					
+					val buttonPadding by animateDpAsState(
+						targetValue = if (isAdvancedButtonExpanded) 6.dp
+						else 8.dp
+					)
+					
+					val buttonSize by animateDpAsState(
+						targetValue = if (isAdvancedButtonExpanded) componentSize.calcCButton.width
+							.minus(componentSize.calcCButton.width.div(4))
+						else componentSize.calcCButton.width
+					)
+					
+					Row(
+						verticalAlignment = Alignment.CenterVertically,
+						horizontalArrangement = Arrangement.Center,
 						modifier = Modifier
-							.weight(1f, fill = true)
-							.clickable(
-								enabled = true,
-								indication = null,
+							.fillMaxWidth()
+					) {
+						Box(
+							contentAlignment = Alignment.Center,
+							modifier = Modifier
+								.weight(1f, fill = true)
+								.clickable(
+									enabled = true,
+									indication = null,
+									interactionSource = r1InteractionSource,
+									onClick = updateExpressionR1
+								)
+						) {
+							Card(
+								shape = fully_rounded,
 								interactionSource = r1InteractionSource,
-								onClick = updateExpressionR1
-							)
-					) {
-						Card(
-							shape = fully_rounded,
-							interactionSource = r1InteractionSource,
-							colors = CardDefaults.cardColors(
-								containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
-									enabled = true
-								).value else Color.Transparent
-							),
-							onClick = updateExpressionR1,
-							modifier = Modifier
-								.padding(8.dp)
-								.size(componentSize.calcCButton.width)
-						) {
-							Box(
-								contentAlignment = Alignment.Center,
+								colors = CardDefaults.cardColors(
+									containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
+										enabled = true
+									).value else Color.Transparent
+								),
+								onClick = updateExpressionR1,
 								modifier = Modifier
-									.fillMaxSize()
+									.padding(buttonPadding)
+									.size(buttonSize)
 							) {
-								Text(
-									text = r1.symbol,
-									style = MaterialTheme.typography.titleLarge.copy(
-										fontWeight = FontWeight.Bold
-									)
-								)
-							}
-						}
-					}
-					
-					Box(
-						contentAlignment = Alignment.Center,
-						modifier = Modifier
-							.weight(1f, fill = true)
-							.clickable(
-								enabled = true,
-								indication = null,
-								interactionSource = r2InteractionSource,
-								onClick = updateExpressionR2
-							)
-					) {
-						Card(
-							shape = fully_rounded,
-							interactionSource = r2InteractionSource,
-							colors = CardDefaults.cardColors(
-								containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
-									enabled = true
-								).value else Color.Transparent
-							),
-							onClick = updateExpressionR2,
-							modifier = Modifier
-								.padding(8.dp)
-								.size(componentSize.calcCButton.width)
-						) {
-							Box(
-								contentAlignment = Alignment.Center,
-								modifier = Modifier
-									.fillMaxSize()
-							) {
-								Text(
-									text = r2.symbol,
-									style = MaterialTheme.typography.titleLarge.copy(
-										fontWeight = FontWeight.Bold
-									)
-								)
-							}
-						}
-					}
-					
-					Box(
-						contentAlignment = Alignment.Center,
-						modifier = Modifier
-							.weight(1f, fill = true)
-							.clickable(
-								enabled = true,
-								indication = null,
-								interactionSource = r3InteractionSource,
-								onClick = updateExpressionR3
-							)
-					) {
-						Card(
-							shape = fully_rounded,
-							interactionSource = r3InteractionSource,
-							colors = CardDefaults.cardColors(
-								containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
-									enabled = true
-								).value else Color.Transparent
-							),
-							onClick = updateExpressionR3,
-							modifier = Modifier
-								.padding(8.dp)
-								.size(componentSize.calcCButton.width)
-						) {
-							Box(
-								contentAlignment = Alignment.Center,
-								modifier = Modifier
-									.fillMaxSize()
-							) {
-								if (r3 == CalcCAction.Delete()) {
-									Icon(
-										painter = painterResource(id = R.drawable.ic_delete),
-										contentDescription = null
-									)
-								} else {
+								Box(
+									contentAlignment = Alignment.Center,
+									modifier = Modifier
+										.fillMaxSize()
+								) {
 									Text(
-										text = list[2]!!.symbol,
+										text = r1.symbol,
 										style = MaterialTheme.typography.titleLarge.copy(
 											fontWeight = FontWeight.Bold
 										)
@@ -308,39 +271,124 @@ fun MainScreen() {
 								}
 							}
 						}
-					}
-					
-					Box(
-						contentAlignment = Alignment.Center,
-						modifier = Modifier
-							.weight(1f, fill = true)
-							.clickable(
-								enabled = true,
-								indication = null,
-								interactionSource = r4InteractionSource,
-								onClick = updateExpressionR4
-							)
-					) {
-						Card(
-							shape = fully_rounded,
-							interactionSource = r4InteractionSource,
-							colors = CardDefaults.cardColors(),
-							onClick = updateExpressionR4,
+						
+						Box(
+							contentAlignment = Alignment.Center,
 							modifier = Modifier
-								.padding(8.dp)
-								.size(componentSize.calcCButton.width)
-						) {
-							Box(
-								contentAlignment = Alignment.Center,
-								modifier = Modifier
-									.fillMaxSize()
-							) {
-								Text(
-									text = r4.symbol,
-									style = MaterialTheme.typography.titleLarge.copy(
-										fontWeight = FontWeight.Bold
-									)
+								.weight(1f, fill = true)
+								.clickable(
+									enabled = true,
+									indication = null,
+									interactionSource = r2InteractionSource,
+									onClick = updateExpressionR2
 								)
+						) {
+							Card(
+								shape = fully_rounded,
+								interactionSource = r2InteractionSource,
+								colors = CardDefaults.cardColors(
+									containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
+										enabled = true
+									).value else Color.Transparent
+								),
+								onClick = updateExpressionR2,
+								modifier = Modifier
+									.padding(buttonPadding)
+									.size(buttonSize)
+							) {
+								Box(
+									contentAlignment = Alignment.Center,
+									modifier = Modifier
+										.fillMaxSize()
+								) {
+									Text(
+										text = r2.symbol,
+										style = MaterialTheme.typography.titleLarge.copy(
+											fontWeight = FontWeight.Bold
+										)
+									)
+								}
+							}
+						}
+						
+						Box(
+							contentAlignment = Alignment.Center,
+							modifier = Modifier
+								.weight(1f, fill = true)
+								.clickable(
+									enabled = true,
+									indication = null,
+									interactionSource = r3InteractionSource,
+									onClick = updateExpressionR3
+								)
+						) {
+							Card(
+								shape = fully_rounded,
+								interactionSource = r3InteractionSource,
+								colors = CardDefaults.cardColors(
+									containerColor = if (i == 0) CardDefaults.cardColors().containerColor(
+										enabled = true
+									).value else Color.Transparent
+								),
+								onClick = updateExpressionR3,
+								modifier = Modifier
+									.padding(buttonPadding)
+									.size(buttonSize)
+							) {
+								Box(
+									contentAlignment = Alignment.Center,
+									modifier = Modifier
+										.fillMaxSize()
+								) {
+									if (r3 == CalcCAction.Delete()) {
+										Icon(
+											painter = painterResource(id = R.drawable.ic_delete),
+											contentDescription = null
+										)
+									} else {
+										Text(
+											text = list[2]!!.symbol,
+											style = MaterialTheme.typography.titleLarge.copy(
+												fontWeight = FontWeight.Bold
+											)
+										)
+									}
+								}
+							}
+						}
+						
+						Box(
+							contentAlignment = Alignment.Center,
+							modifier = Modifier
+								.weight(1f, fill = true)
+								.clickable(
+									enabled = true,
+									indication = null,
+									interactionSource = r4InteractionSource,
+									onClick = updateExpressionR4
+								)
+						) {
+							Card(
+								shape = fully_rounded,
+								interactionSource = r4InteractionSource,
+								colors = CardDefaults.cardColors(),
+								onClick = updateExpressionR4,
+								modifier = Modifier
+									.padding(buttonPadding)
+									.size(buttonSize)
+							) {
+								Box(
+									contentAlignment = Alignment.Center,
+									modifier = Modifier
+										.fillMaxSize()
+								) {
+									Text(
+										text = r4.symbol,
+										style = MaterialTheme.typography.titleLarge.copy(
+											fontWeight = FontWeight.Bold
+										)
+									)
+								}
 							}
 						}
 					}
